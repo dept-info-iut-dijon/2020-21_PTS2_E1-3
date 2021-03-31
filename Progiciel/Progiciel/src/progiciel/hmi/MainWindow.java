c76de8ccb13f8b2df898735797c6f9d5d480bf2c
ValentinM27 <74249732+ValentinM27@users.noreply.github.com>
2021-03-31 18:27:40 +0200

    MainWindow
    MainWindow lorsque l'utilisateur n'est pas connecté

--- Progiciel/Progiciel/src/progiciel/hmi/MainWindow.java
+++ Progiciel/Progiciel/src/progiciel/hmi/MainWindow.java
@@ -27,24 +27,190 @@ public class MainWindow extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        logginBtn = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        supportBtn = new javax.swing.JButton();
+        logo = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        homeMenu = new javax.swing.JMenu();
+        homeBtn = new javax.swing.JMenuItem();
+        techBtn = new javax.swing.JMenuItem();
+        projectsBtn = new javax.swing.JMenuItem();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Progiciel");
         setMinimumSize(new java.awt.Dimension(1280, 720));
-        setPreferredSize(new java.awt.Dimension(1280, 720));
         setResizable(false);
+
+        jPanel1.setBackground(new java.awt.Color(46, 48, 47));
+
+        jLabel1.setFont(new java.awt.Font("Gill Sans MT", 1, 48)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("Welcome");
+
+        jLabel2.setFont(new java.awt.Font("Dialog", 1, 36)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("Please Log In ");
+
+        logginBtn.setBackground(new java.awt.Color(237, 132, 38));
+        logginBtn.setFont(new java.awt.Font("Gill Sans MT", 1, 24)); // NOI18N
+        logginBtn.setForeground(new java.awt.Color(255, 255, 255));
+        logginBtn.setText("Log In");
+        logginBtn.setBorderPainted(false);
+        logginBtn.setFocusPainted(false);
+        logginBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                logginBtnActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setFont(new java.awt.Font("Brush Script MT", 1, 36)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("You are not connected:");
+
+        jPanel2.setBackground(new java.awt.Color(153, 153, 153));
+
+        jLabel4.setFont(new java.awt.Font("Gill Sans MT", 1, 12)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel4.setText("Progiciel V0.1 for MSP gmBH.");
+
+        jLabel5.setFont(new java.awt.Font("Gill Sans MT", 1, 12)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel5.setText("© 2021 Jakova.All rigths reserved");
+
+        supportBtn.setBackground(new java.awt.Color(153, 153, 153));
+        supportBtn.setFont(new java.awt.Font("Gill Sans MT", 1, 12)); // NOI18N
+        supportBtn.setForeground(new java.awt.Color(0, 0, 0));
+        supportBtn.setText("Support");
+        supportBtn.setBorderPainted(false);
+        supportBtn.setFocusPainted(false);
+        supportBtn.setOpaque(false);
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(supportBtn)
+                .addGap(28, 28, 28)
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel5)
+                .addGap(63, 63, 63))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(supportBtn)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel5)))
+        );
+
+        logo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/progiciel/hmi/images/logo.png"))); // NOI18N
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(520, 520, 520)
+                        .addComponent(jLabel2))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(16, 16, 16)
+                        .addComponent(logo)
+                        .addGap(401, 401, 401)
+                        .addComponent(jLabel1)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGap(0, 481, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addGap(481, 481, 481))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(logginBtn)
+                        .addGap(576, 576, 576))))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel1)
+                    .addComponent(logo))
+                .addGap(119, 119, 119)
+                .addComponent(jLabel3)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel2)
+                .addGap(18, 18, 18)
+                .addComponent(logginBtn)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        jMenuBar1.setBackground(new java.awt.Color(102, 102, 102));
+        jMenuBar1.setBorder(null);
+        jMenuBar1.setForeground(new java.awt.Color(102, 102, 102));
+        jMenuBar1.setOpaque(false);
+
+        homeMenu.setBackground(new java.awt.Color(153, 153, 153));
+        homeMenu.setForeground(new java.awt.Color(0, 0, 0));
+        homeMenu.setText("Menu");
+        homeMenu.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                homeMenuActionPerformed(evt);
+            }
+        });
+
+        homeBtn.setText("Home");
+        homeMenu.add(homeBtn);
+
+        techBtn.setText("Techniciens ");
+        homeMenu.add(techBtn);
+
+        projectsBtn.setText("Projects");
+        homeMenu.add(projectsBtn);
+
+        jMenuBar1.add(homeMenu);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1280, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 720, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void homeMenuActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_homeMenuActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_homeMenuActionPerformed
+
+    private void logginBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_logginBtnActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_logginBtnActionPerformed
 
     /**
      * @param args the command line arguments
@@ -82,5 +248,20 @@ public class MainWindow extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem homeBtn;
+    private javax.swing.JMenu homeMenu;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JButton logginBtn;
+    private javax.swing.JLabel logo;
+    private javax.swing.JMenuItem projectsBtn;
+    private javax.swing.JButton supportBtn;
+    private javax.swing.JMenuItem techBtn;
     // End of variables declaration//GEN-END:variables
 }
