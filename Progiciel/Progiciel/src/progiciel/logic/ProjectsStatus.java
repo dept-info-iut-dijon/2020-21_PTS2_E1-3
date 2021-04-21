7a5ebbb1d57944039237782979beeff9dd16b96c
ValentinM27 <74249732+ValentinM27@users.noreply.github.com>
2021-04-21 13:29:49 +0200

    Enum ProjectStatus

---
+++ Progiciel/Progiciel/src/progiciel/logic/ProjectsStatus.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progiciel.logic;
+
+/**
+ *  Énumération des status possibles pour un projet 
+ * @author margu
+ */
+public enum ProjectsStatus {
+    ENDED,
+    WAITING,
+    WORKING,
+    CANCELED
+}
